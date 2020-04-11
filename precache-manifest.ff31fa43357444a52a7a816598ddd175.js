f85037851592b6917f23911e11e2de096ffb6796
Darshan Baral <darshanbaral@gmail.com>
2020-04-11 11:30:09 -0700

    Updates

---
+++ precache-manifest.ff31fa43357444a52a7a816598ddd175.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66533f01662d32654643",
+    "url": "/dna/static/css/main.331e7a84.chunk.css"
+  },
+  {
+    "revision": "66533f01662d32654643",
+    "url": "/dna/static/js/main.66533f01.chunk.js"
+  },
+  {
+    "revision": "28a2ca5f5b9973eb48dc",
+    "url": "/dna/static/js/1.28a2ca5f.chunk.js"
+  },
+  {
+    "revision": "82acc9a538b4a667c7f4",
+    "url": "/dna/static/js/runtime~main.82acc9a5.js"
+  },
+  {
+    "revision": "0873a92b5b15ccc8c133af99791ac1f0",
+    "url": "/dna/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dna/precache-manifest.c16233b090554c3125e24e482d2bf404.js"
+  "/dna/precache-manifest.ff31fa43357444a52a7a816598ddd175.js"
 );
 
 workbox.clientsClaim();
