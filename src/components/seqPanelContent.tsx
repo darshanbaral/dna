a031e93430b3d2c8dcbaa54bfd9d4fdf0a61e468
Darshan Baral <darshanbaral@gmail.com>
2020-04-11 20:55:29 -0700

    Added a message for copying seqs

--- src/components/seqPanelContent.tsx
+++ src/components/seqPanelContent.tsx
@@ -4,6 +4,31 @@ import CopyToClipboard from "react-copy-to-clipboard";
 import IconButton from "@material-ui/core/IconButton";
 
 export default function SeqPanelContent(props: any) {
+  const [disp, setDisp] = React.useState("hidden");
+
+  React.useEffect(() => {
+    if (disp === "visible") {
+      setTimeout(() => setDisp("hidden"), 1750);
+    }
+  }, [disp]);
+
+  const handleClick = () => {
+    setDisp("visible");
+  };
+
+  const msgStyle = {
+    backgroundColor: "#edf7ed",
+    border: "solid 1px #5cb660",
+    textAlign: "center",
+    borderRadius: "3px",
+    fontSize: "0.8em",
+    padding: "2px",
+    margin: "0",
+    visibility: disp,
+    opacity: disp === "hidden" ? 0 : 1,
+    transition: "all 0.5s",
+  } as React.CSSProperties;
+
   const makeNRed = (seq: string) => {
     return seq.split("").map((el: string) => {
       return (
@@ -24,9 +49,10 @@ export default function SeqPanelContent(props: any) {
       >
         <CopyToClipboard text={props.val}>
           <IconButton aria-label="copy" color="primary">
-            <FileCopyOutlinedIcon />
+            <FileCopyOutlinedIcon onClick={handleClick} />
           </IconButton>
         </CopyToClipboard>
+        <p style={msgStyle}>Copied</p>
       </div>
     </div>
   ) : null;
